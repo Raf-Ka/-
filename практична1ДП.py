0a032ea2363cd1ace25378d5c7c6351625e827b9
Raf-Ka <pdiman165@gmail.com>
2024-09-16 13:34:04 +0300

    Add files via upload

---
+++ практична1ДП.py
@@ -0,0 +1,9 @@
+world = "Hellow world"
+first_name = "Дмітрій"
+last_name = "Поляков"
+age = 16
+
+print("Тип данний змінної світ : ",type(world))
+print("Тип данний змінної ім'я : ",type(first_name))
+print("Тип данний змінної вік : ",type(age))
+print("Тип данний змінної призвіщем : ",type(last_name))
